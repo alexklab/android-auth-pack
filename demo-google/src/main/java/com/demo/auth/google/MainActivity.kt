f6986c165ed39932b7dbfdf8a4f993ad4229f9c7
alexk <gklymoxa@gmail.com>
2019-02-28 11:00:21 +0200

    Initial commit for demo-google module

---
+++ demo-google/src/main/java/com/demo/auth/google/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.demo.auth.google
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
