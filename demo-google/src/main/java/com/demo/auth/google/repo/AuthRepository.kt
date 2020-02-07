60e14ae76ab6273de93fcc45724704384cfc6a60
alexk <gklymoxa@gmail.com>
2020-02-07 16:13:42 +0200

    minor fix

--- demo-google/src/main/java/com/demo/auth/google/repo/AuthRepository.kt
+++ demo-google/src/main/java/com/demo/auth/google/repo/AuthRepository.kt
@@ -1,22 +1,15 @@
 package com.demo.auth.google.repo
 
-import androidx.activity.ComponentActivity
 import com.android.arch.auth.core.data.repository.SingleAuthRepository
-import com.android.arch.auth.core.data.repository.SocialNetworkAuthRepository
 import com.android.arch.auth.google.GoogleSignInService
 import com.demo.auth.google.db.UserProfile
 import timber.log.Timber
 
-class AuthRepository(private val signInService: GoogleSignInService) :
-    SingleAuthRepository<UserProfile>(::UserProfile),
-    SocialNetworkAuthRepository<UserProfile> {
+class AuthRepository(signInService: GoogleSignInService) :
+    SingleAuthRepository<UserProfile>(::UserProfile, signInService) {
 
     init {
         Timber.d("init instance $this")
     }
 
-    override fun onCreate(activity: ComponentActivity) {
-        Timber.d("onCreate: activity=$activity, this=$this")
-        super.onCreate(activity, signInService)
-    }
 }
