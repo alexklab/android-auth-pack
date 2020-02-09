43798066dad6db1cca9499281c0a29b0cc6f2157
alexk <gklymoxa@gmail.com>
2020-02-09 16:30:06 +0200

    demo instagram : fix support new api

--- demo-instagram/src/main/java/com/demo/auth/instagram/MainActivity.kt
+++ demo-instagram/src/main/java/com/demo/auth/instagram/MainActivity.kt
@@ -4,31 +4,41 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.arch.auth.core.data.entity.SignInResponse
+import com.android.arch.auth.core.data.entity.SocialNetworkType
+import com.android.arch.auth.core.data.network.SignInServiceListener
 import com.android.arch.instagram.InstagramSignInService
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SignInServiceListener {
+
+    private lateinit var instagramSignInService: InstagramSignInService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val service = InstagramSignInService(
+        instagramSignInService = InstagramSignInService(
             clientId = getString(R.string.instagram_client_id),
             redirectUrl = getString(R.string.instagram_redirect_url)
-        )
-
-        service.onCreate(this)
-
-        signInButton.setOnClickListener {
-            service.signIn { (account, error) ->
-                val msg = if (account != null) {
-                    "SignIn Success: $account"
-                } else {
-                    "SignIn Error: $error"
-                }
-                Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
-                Log.d("signIn Response", msg)
-            }
+        ).also {
+            it.onCreate(this@MainActivity)
+            it.addListener(this@MainActivity)
         }
+
+        signInButton.setOnClickListener { instagramSignInService.signIn() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        instagramSignInService.removeListener(this@MainActivity)
+    }
+
+    override fun onSignInResponse(socialNetwork: SocialNetworkType, response: SignInResponse) {
+        val (profile, error) = response
+        val msg = if (profile != null) "Success: $profile" else "Error: $error"
+        Log.d("MainActivity", "onSignInResponse: $msg", error?.exception)
+        Toast
+            .makeText(this@MainActivity, "SignIn Response: $msg", Toast.LENGTH_LONG)
+            .show()
     }
 }
