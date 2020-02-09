e58c4fb45b9b37137f5df719288b8f20046afa0a
alexk <gklymoxa@gmail.com>
2020-02-09 16:44:09 +0200

    update build versions

--- demo-firebase/src/main/java/com/demo/auth/firebase/common/PasswordLayoutListener.kt
+++ demo-firebase/src/main/java/com/demo/auth/firebase/common/PasswordLayoutListener.kt
@@ -2,11 +2,14 @@ package com.demo.auth.firebase.common
 
 import android.view.View
 import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.textfield.TextInputLayout.END_ICON_NONE
+import com.google.android.material.textfield.TextInputLayout.END_ICON_PASSWORD_TOGGLE
 
-class PasswordLayoutListener(private val passwordLayout: TextInputLayout) : View.OnFocusChangeListener {
+class PasswordLayoutListener(private val passwordLayout: TextInputLayout) :
+    View.OnFocusChangeListener {
 
     override fun onFocusChange(v: View?, hasFocus: Boolean): Unit = with(passwordLayout) {
         if (!hasFocus) error = null
-        isPasswordVisibilityToggleEnabled = hasFocus
+        endIconMode = if (hasFocus) END_ICON_PASSWORD_TOGGLE else END_ICON_NONE
     }
 }
